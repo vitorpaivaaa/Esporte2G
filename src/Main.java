8b8fd10e34c48ba2b9ac5c23bc1d9d7c132a2a78
Vitor Paiva <vp33435121@gmail.com>
2023-11-09 07:52:20 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Pessoa qualquer = new Pessoa();
+        qualquer.setNome("Miters");
+        System.out.println(qualquer.getNome());
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+
+public class Pessoa {
+    //atributos
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private double peso;
+    private double altura;
+
+    // getters and setters
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
